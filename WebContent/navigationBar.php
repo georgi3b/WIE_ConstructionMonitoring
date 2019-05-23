516432a00fa8156bfeebb059a0ce4ccd170a76fc
georgi3b <budgeorgiana@gmail.com>
2019-05-23 14:34:58 +0200

    navbar.php e exampleProjects.php

---
+++ WebContent/navigationBar.php
@@ -0,0 +1,40 @@
+<header>
+		<nav class="navbar navbar-expand-md navbar-dark fixed-top">
+			<a class="navbar-brand" href="exampleHome.html">BuildUp</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse"
+				data-target="#navbarCollapse" aria-controls="navbarCollapse"
+				aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="navbar-collapse collapse" id="navbarCollapse" style="">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active"><a class="nav-link"
+						href="exampleProjects.html"> Projects</a></li>
+					<li class="nav-item dropdown"><a
+						class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+						role="button" data-toggle="dropdown" aria-haspopup="true"
+						aria-expanded="false">Monitoring</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Swiss Re</a> <a
+								class="dropdown-item" href="#">IOC</a> <a class="dropdown-item"
+								href="#">Unibx</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="exampleProjects.html">All
+								projects</a>
+						</div></li>
+					<li class="nav-item"><a class="nav-link"
+						href="exampleProfile.html">My Profile</a></li>
+					<li class="nav-item"><a class="nav-link"
+						href="exampleAboutUs.html">About Us</a></li>
+
+				</ul>
+				<div class="form-inline mt-2 mt-md-0">
+					<form>
+						<input class="form-control mr-sm-2" type="search"
+							placeholder="Search" aria-label="Search">
+						<!-- button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button-->
+					</form>
+				</div>
+			</div>
+		</nav>
+	</header>
